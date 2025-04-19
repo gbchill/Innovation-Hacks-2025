6a42f313937565a9967a77fc3c6f8e6d7d3e6aa9
Aryan Patel <productivearyan123@gmail.com>
2025-04-19 13:39:25 -0700

    Create boilerplate code

---
+++ WorkNest/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // This tells Electron where your frontend is during development
+  mainWindow.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
